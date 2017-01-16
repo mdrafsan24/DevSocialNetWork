012ccbc5bff5da47a4bcd747ad2c56a30d1c5f2a
Rafsan Chowdhury <rafsanchowdhury@Rafsans-MacBook-Air.local>
2017-01-16 01:36:55 -0500

    FB Authentication Integrated

---
+++ DevSocial/Constants.swift
@@ -0,0 +1,9 @@
+//
+//  Constants.swift
+//  DevSocial
+//
+//  Created by Rafsan Chowdhury on 1/14/17.
+//  Copyright © 2017 Mcraf. All rights reserved.
+//
+
+import Foundation
---
+++ DevSocial/FancyView.swift
@@ -0,0 +1,21 @@
+//
+//  FancyView.swift
+//  DevSocial
+//
+//  Created by Rafsan Chowdhury on 1/14/17.
+//  Copyright © 2017 Mcraf. All rights reserved.
+//
+
+import UIKit
+
+class FancyView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
