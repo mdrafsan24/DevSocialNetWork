af943da1cfa0e774d16ae7e434cbb3e9f224d847
Rafsan Chowdhury <rafsanchowdhury@Rafsans-MacBook-Air.local>
2017-01-22 14:13:57 -0500

    Firebase GET Complete

--- DevSocial/FeedVC.swift
+++ DevSocial/FeedVC.swift
@@ -13,6 +13,8 @@ import SwiftKeychainWrapper
 class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet weak var tableView: UITableView!
+    
+    var posts = [Post]() //Bunch od posts
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,7 +22,20 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         tableView.dataSource = self
         
         DataService.ds.REF_POSTS.observe(.value, with: { (snapshot) in
-            print(snapshot.value)
+            //print(snapshot.value) // Printing the data Printed out 2 posts
+            
+            if let snapshot = snapshot.children.allObjects as? [FIRDataSnapshot] {
+                for snap in snapshot {
+                    print("SNAP: \(snap)")
+                    if let postDict = snap.value as? Dictionary<String, AnyObject> {
+                        let key = snap.key
+                        let post = Post(postKey: key, postData: postDict)
+                        self.posts.append(post) //Adding to posts array
+                    
+                    }
+                }
+            }
+            self.tableView.reloadData() //
         })
     }
     
@@ -29,10 +44,13 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return posts.count //Number of objects
     }
     
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let post = posts[indexPath.row]
+        print("Jess: \(post.caption)")
         return tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell
     }
         
